dfbfb4a990d78346308822d18e69a45e0db18be6
test <test@pganjac.com>
2022-10-15 16:08:14 +0530

    mymessage

---
+++ models/books.model.js
@@ -0,0 +1,11 @@
+const book = [
+    {
+        id: 0,
+        book_name : "The Psychology Of Women",
+        book_id : "TI-100",
+        book_url : "https://www.oxfordbibliographies.com/view/document/obo-9780199828340/obo-9780199828340-0046.xml#:~:text=The%20psychology%20of%20women%20addresses,nonverbal%20communications%20by%20and%20about",
+        book_img : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.in%2FPsychology-Women-Handbook-Issues-Theories-ebook%2Fdp%2FB00EQ3AKU2&psig=AOvVaw11_E5fC6UQZaH3QiEY23aA&ust=1665916104783000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCJCax5uC4voCFQAAAAAdAAAAABAD",
+    },
+
+];
+module.exports = books;
